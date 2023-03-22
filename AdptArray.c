e56a266dca625f27b2f0d2238c5e81acbadbe1b1
ozkfir <118678829+ozkfir@users.noreply.github.com>
2023-03-22 11:50:53 +0200

    Add files via upload

---
+++ AdptArray.c
@@ -0,0 +1,102 @@
+#include "Person.h"
+#include "book.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "AdptArray.h"
+
+
+typedef struct AdptArray_{
+    int size;
+    DEL_FUNC f_del;
+    COPY_FUNC f_copy;
+    PRINT_FUNC f_print;
+    PElement * arr;
+    int live;
+}AdptArray;
+
+
+int bigerr_arr(PAdptArray adpt,int i)
+{
+    adpt->arr=(PElement*)realloc(adpt->arr,(i+1)* sizeof(PElement));
+    if (adpt->arr == NULL){
+        printf("problem with memory");
+        return 0;
+    }
+    for (int j = adpt->size; j <=i ; j++) {
+        adpt->arr[j]=NULL;
+    }
+    adpt->size=i+1;
+    return 1;
+}
+
+
+
+
+PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC delFunc,PRINT_FUNC printFunc){
+    PAdptArray adpt = NULL;
+    adpt = (PAdptArray)calloc(1, sizeof(AdptArray));
+    if (adpt == NULL) {
+        printf("problem with memory");
+        exit(1);
+    }
+    adpt->size=0;
+    adpt->arr = (PElement*)calloc(1, sizeof(PElement));
+    if (adpt->arr == NULL) {
+        printf("problem with memory");
+        free(adpt);
+        exit(1);
+    }
+    adpt->f_del=delFunc;
+    adpt->f_copy=copyFunc;
+    adpt->f_print=printFunc;
+    return adpt;
+}
+void DeleteAdptArray(PAdptArray adpt){
+    if(adpt!=NULL) {
+        if (adpt->arr != NULL) {
+            for (int i = 0; i < adpt->size; i++) {
+                if (adpt->arr[i] != NULL)
+                    adpt->f_del(adpt->arr[i]);
+            }
+            free(adpt->arr);
+        }
+    free(adpt);
+    }
+}
+Result SetAdptArrayAt(PAdptArray adpt, int i, PElement pElement){////how can fail
+    if(adpt->size<=i)
+        if(!bigerr_arr(adpt,i))
+            return FAIL;
+    if((adpt->arr[i])!=NULL)
+        free(adpt->arr[i]);
+    adpt->arr[i]=adpt->f_copy(pElement);
+    return SUCCESS;
+}
+
+
+
+PElement GetAdptArrayAt(PAdptArray adpt, int i)
+{
+    if(adpt->size <= i)
+    {
+     return NULL;
+    }
+    if((adpt->arr)[i]==NULL)
+        return NULL;
+    return adpt->f_copy(adpt->arr[i]);
+}
+
+
+
+int GetAdptArraySize(PAdptArray adpt)
+{
+    return adpt->size;
+}
+
+
+void PrintDB(PAdptArray adpt){
+    for (int i = 0; i < adpt->size; i++)
+        if (adpt->arr[i] != NULL)
+            adpt->f_print(adpt->arr[i]);
+}
+
